f2637c06d8a1d39f59d2af657ee5998da82cb4ce
BeatDiaz <beatdiazmusic@gmail.com>
2025-06-30 11:55:05 +0300

    question 1

--- jsAsyncExe.js
+++ jsAsyncExe.js
@@ -1,1 +1,20 @@
 //1
+// import fs from "fs";
+
+// fs.readFile("./aaa.txt", (err, data) => {
+//     if (err) {
+//         console.log("err");
+//         return 
+//     }
+//     const d = data;
+
+//     fs.stat("./aaa.txt", (err, stats) => {
+//         if (err) {       
+//             console.log("err");
+//             return
+//         }
+//         console.log("\nfile info: ");
+//         console.log(stats.size);
+//         console.log(stats.birthtime);  
+//     });
+// });
